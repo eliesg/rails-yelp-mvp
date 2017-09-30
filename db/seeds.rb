24cc60b609c469d7b53e61f5ff39bc614e7336f3
Elies Gaillard <elies.gaillard@gmail.com>
2017-09-30 17:32:47 +0200

    exo

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,44 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         "Crossways",
+    address:      "123 Swanston St, Melbourne",
+    phone_number: "01 43 54 23 36",
+    category:     "chinese"
+  },
+  {
+    name:         "Lentils as Anything",
+    address:      "Brunswick St, Saint-Kilda, Melbourne",
+    phone_number: "01 43 54 23 31",
+    category:     "chinese"
+  },
+  {
+    name:         "La truffière",
+    address:      "4 rue Blainville 75005 Paris",
+    phone_number: "01 43 54 23 32",
+    category:     "french"
+  },
+  {
+    name:         "Le pré catelan",
+    address:      "route de Suresnes 75016 Paris",
+    phone_number: "01 43 54 23 33",
+    category:     "french"
+  },
+  {
+    name:         "Le Solfé",
+    address:      "rue Solférino 75016 Paris",
+    phone_number: "01 43 54 23 34",
+    category:     "italian"
+  },
+  {
+    name:         "La Gueuze",
+    address:      "route de Marseille 75016 Paris",
+    phone_number: "01 43 54 23 35",
+    category:     "belgian"
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
